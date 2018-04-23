36c381aee25468aa5fac04a56a2028c0d2d58a9d
Ignat Ilie <ilya_ignat@icloud.com>
2018-04-23 21:38:06 +0300

    Microchange performed.

--- app/src/main/java/md/luciddream/findaid/MainActivity.java
+++ app/src/main/java/md/luciddream/findaid/MainActivity.java
@@ -2,6 +2,8 @@ package md.luciddream.findaid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onMapClick(View view){
+        Toast.makeText(view.getContext(), "HI", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/test/java/md/luciddream/findaid/MainActivityTest.java
+++ app/src/test/java/md/luciddream/findaid/MainActivityTest.java
@@ -1,4 +1,13 @@
 package md.luciddream.findaid;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MainActivityTest {
+    @Test
+    public void onClick_MapOnClick_Test(){
+
+    }
 }
