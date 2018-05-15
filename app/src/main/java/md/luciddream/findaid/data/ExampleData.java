4ac49187b4ff85943638376801c4dd4dad9cbddf
ignatilie <iignat@tacitknowledge.com>
2018-05-15 16:50:02 +0300

    Filled some extra example data.

--- app/src/main/java/md/luciddream/findaid/data/ExampleData.java
+++ app/src/main/java/md/luciddream/findaid/data/ExampleData.java
@@ -1,6 +1,11 @@
 package md.luciddream.findaid.data;
 
+import android.util.Pair;
 import md.luciddream.findaid.data.model.*;
+import md.luciddream.findaid.data.model.join.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ExampleData {
     Location[] locations;
@@ -9,7 +14,13 @@ public class ExampleData {
     Step[] steps;
     Symptom[] symptoms;
     Trauma[] traumas;
-    ExampleData(){
+    TraumaLocation[] traumaLocations;
+    TraumaOrgan[] traumaOrgans;
+    TraumaSeason[] traumaSeasons;
+    TraumaStep[] traumaSteps;
+    TraumaSymptom[] traumaSymptoms;
+
+    ExampleData() {
         locations = new Location[]{new Location(null, "Mountains"),
                 new Location(null, "Seashore"),
                 new Location(null, "Sea"),
@@ -22,26 +33,82 @@ public class ExampleData {
                 new Organ(null, "Arm"),
                 new Organ(null, "Leg")};
 
-        seasons = new Season[]{new Season(null , "Winter"),
-                new Season(null , "Spring"),
-                new Season(null , "Summer"),
-                new Season(null , "Autumn")};
+        seasons = new Season[]{new Season(null, "Winter"),
+                new Season(null, "Spring"),
+                new Season(null, "Summer"),
+                new Season(null, "Autumn")};
 
         steps = new Step[]{new Step(null, "Call ambulance"),
                 new Step(null, "Find water"),
                 new Step(null, "Give water"),
                 new Step(null, "Find bandage"),
-                new Step(null, "Use bandage")};
+                new Step(null, "Use bandage"),
+                new Step(null, "The the person to the shade")};
 
         symptoms = new Symptom[]{new Symptom(null, "Headache"),
                 new Symptom(null, "Dizziness"),
                 new Symptom(null, "Red skin"),
-                new Symptom(null, "Red eyes")};
+                new Symptom(null, "Red eyes"),
+                new Symptom(null, "Sharp ache in limb or joint"),
+                new Symptom(null, "Nausea")};
 
         traumas = new Trauma[]{new Trauma(null, "Sunburn", 10),
                 new Trauma(null, "Sunstroke", 10),
                 new Trauma(null, "Luxation", 5),
                 new Trauma(null, "Burn", 10)};
+        traumaLocations = new TraumaLocation[]{
+                new TraumaLocation(3, 1),
+                new TraumaLocation(4, 2),
+                new TraumaLocation(1, 3),
+                new TraumaLocation(2, 4)
+        };
+        traumaOrgans = new TraumaOrgan[]{
+                new TraumaOrgan(1, 1),
+                new TraumaOrgan(2, 2),
+                new TraumaOrgan(4, 3),
+                new TraumaOrgan(1, 4)
+        };
+        traumaSeasons = new TraumaSeason[]{
+                new TraumaSeason(3, 1),
+                new TraumaSeason(3, 2),
+                new TraumaSeason(1, 3),
+                new TraumaSeason(3, 4)
+        };
+        traumaSteps = new TraumaStep[]{
+                new TraumaStep(1, 1, 1),
+                new TraumaStep(2, 1,2 ),
+                new TraumaStep(3, 1, 3),
+
+                new TraumaStep(1, 2,1 ),
+                new TraumaStep(6, 2, 2),
+
+                new TraumaStep(2, 3, 1),
+                new TraumaStep(5, 3, 2),
+
+                new TraumaStep(1, 4, 1),
+                new TraumaStep(4, 4, 2),
+                new TraumaStep(5, 4, 3)
+        };
+
+        traumaSymptoms = new TraumaSymptom[]{
+                new TraumaSymptom(1, 1),
+                new TraumaSymptom(2, 1),
+                new TraumaSymptom(3, 1),
+
+                new TraumaSymptom(1, 2),
+                new TraumaSymptom(2, 2),
+                new TraumaSymptom(6, 2),
+
+                new TraumaSymptom(1, 3),
+                new TraumaSymptom(4, 3),
+                new TraumaSymptom(5, 3),
+
+                new TraumaSymptom(1, 4),
+                new TraumaSymptom(2, 4),
+                new TraumaSymptom(3, 4)
+        };
+
+
     }
 
     public Location[] getLocations() {
@@ -66,5 +133,25 @@ public class ExampleData {
 
     public Trauma[] getTraumas() {
         return traumas;
+    }
+
+    public TraumaLocation[] getTraumaLocations() {
+        return traumaLocations;
+    }
+
+    public TraumaOrgan[] getTraumaOrgans() {
+        return traumaOrgans;
+    }
+
+    public TraumaSeason[] getTraumaSeasons() {
+        return traumaSeasons;
+    }
+
+    public TraumaStep[] getTraumaSteps() {
+        return traumaSteps;
+    }
+
+    public TraumaSymptom[] getTraumaSymptoms() {
+        return traumaSymptoms;
     }
 }
--- app/src/main/java/md/luciddream/findaid/data/FindAidDatabase.java
+++ app/src/main/java/md/luciddream/findaid/data/FindAidDatabase.java
@@ -6,12 +6,14 @@ import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Pair;
 import md.luciddream.findaid.data.dao.*;
 import md.luciddream.findaid.data.dao.join.*;
 import md.luciddream.findaid.data.model.*;
 import md.luciddream.findaid.data.model.join.*;
 
 import javax.inject.Inject;
+import java.util.List;
 import java.util.concurrent.Executors;
 
 
@@ -61,46 +63,15 @@ public abstract class  FindAidDatabase extends RoomDatabase {
                                 database.stepDao().insert(exampleData.getSteps());
                                 database.symptomDao().insert(exampleData.getSymptoms());
                                 database.traumaDao().insert(exampleData.getTraumas());
+                                database.traumaLocationDao().insert(exampleData.getTraumaLocations());
+                                database.traumaOrganDao().insert(exampleData.getTraumaOrgans());
+                                database.traumaSeasonDao().insert(exampleData.getTraumaSeasons());
+                                database.traumaStepDao().insert(exampleData.getTraumaSteps());
+                                database.traumaSymptomDao().insert(exampleData.getTraumaSymptoms());
                             }
                         });
                     }
                 })
                 .build();
     }
-
-
-
-//    public static final Migration INSERT_BASIC_DATA = new Migration(1, 2) {
-//        @Override
-//        public void migrate(@NonNull SupportSQLiteDatabase database) {
-//            database.execSQL("insert into locations values(null, \"Mountains\"");
-//            database.execSQL("insert into locations values(null, \"Seashore");
-//            database.execSQL("insert into locations values(null, \"Sea");
-//            database.execSQL("insert into locations values(null, \"Urban");
-//            database.execSQL("insert into organ values(null, \"skin\"");
-//            database.execSQL("insert into organ values(null, \"head\"");
-//            database.execSQL("insert into organ values(null, \"heart\"");
-//            database.execSQL("insert into organ values(null, \"body\"");
-//            database.execSQL("insert into organ values(null, \"arm\"");
-//            database.execSQL("insert into organ values(null, \"leg\"");
-//            database.execSQL("insert into season values (null , \"winter\"");
-//            database.execSQL("insert into season values (null , \"spring\"");
-//            database.execSQL("insert into season values (null , \"summer\"");
-//            database.execSQL("insert into season values (null , \"autumn\"");
-//            database.execSQL("insert into step values (null, \"Call ambulance\"");
-//            database.execSQL("insert into step values (null, \"Find water\"");
-//            database.execSQL("insert into step values (null, \"Give water\"");
-//            database.execSQL("insert into step values (null, \"Find bandage\"");
-//            database.execSQL("insert into step values (null, \"Use bandage\"");
-//            database.execSQL("insert into symptom values(null, \"headache\"");
-//            database.execSQL("insert into symptom values(null, \"dizziness\"");
-//            database.execSQL("insert into symptom values(null, \"red skin\"");
-//            database.execSQL("insert into symptom values(null, \"red eyes\"");
-//            database.execSQL("insert into trauma values(null, \"sunburn\", 10");
-//            database.execSQL("insert into trauma values(null, \"sunstroke\", 10");
-//            database.execSQL("insert into trauma values(null, \"luxation\", 5");
-//            database.execSQL("insert into trauma values(null, \"burn\", 10");
-//
-//        }
-//    };
 }
